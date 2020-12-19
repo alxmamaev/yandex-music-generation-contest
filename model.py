f9730592306374b396c4711a61d6b23d881d7b08
Alexander Mamaev <alxmamaev@ya.ru>
2020-12-19 20:21:43 +0300

    Hotfix

--- model.py
+++ model.py
@@ -1,9 +1,16 @@
 from transformers import T5Config, T5ForConditionalGeneration
 
 def get_model(vocab_size=30000):
-    config = T5Config()
-    config.vocab_size = vocab_size
+    config_encoder = BertConfig()
+    config_decoder = BertConfig()
 
-    model = T5ForConditionalGeneration(config)
+    config_encoder.vocab_size = vocab_size
+    config_decoder.vocab_size = vocab_size
+
+    config_decoder.is_decoder = True
+    config_decoder.add_cross_attention = True
+
+    config = EncoderDecoderConfig.from_encoder_decoder_configs(config_encoder, config_decoder)
+    model = EncoderDecoderModel(config=config)
     
     return model
